6a7423748495ec63c2a312741d0ed20250944574
unknown <Twice@uber-machine.(none)>
2012-07-15 18:26:31 +0700

    changed rendering files

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -1,8 +1,11 @@
 #include "Renderer.h"
-
+#include "Ray.h"
 Renderer::Renderer()
 {
 	samples = 0;
+	curDepth = 0;
+	distance = 1;
+	mci = new MonteCarloImage();
 }
 
 void Renderer::setBackgroundColor(Vec3<float>& vec)
@@ -30,8 +33,10 @@ void Renderer::render(Camera & camera, Scene & scene)
 	for(int y = 0; y < camera.getDpiY(); y++)
 		for(int x = 0; x < camera.getDpiX(); x++)
 		{
-			Ray<float> ray(camera.getPos(), camera.getDir().normalize());
-			scene.push(ray);
+			vec3 curVec(x,y,distance);
+			Ray ray(camera.getPos(), curVec);
+			Vec3 color = pathTrace(ray, scene);
+			mci->add(y,x,color);
 			samples++;
 		}
 }
@@ -43,12 +48,24 @@ int Renderer::getSamples()
 
 Vec3<float> pathTrace(Ray & ray, Scene & scene)
 {
-	Vec3<float> point;
-	if(scene.getIntersection(ray, &point))
+	if(curDepth > pathDepth)
 	{
-		
+		curDepth = 0;
+		return Vec3<float>(0,0,0);
+	}
+	Vec3<float> point;
+	ISurface* surf = scene.getSurface(ray);
+	if(surf->getIntersection(ray, &point))
+	{
+		Vec3<float> normal = surf->getNormal(point);
+		float x = (rand()%normal.x)/100, y = (rand()%normal.y)/100, z = sqrt(1 - x*x - y*y);
+		Vec3<float> randVec(x, y, z);
+		Ray ray(point, randVec);
+		pathTrace(ray,scene);
+		curDepth++;
 	}
 	else {
+		curDepth = 0;
 		return Vec3<float>(0,0,0);
 	}
 	

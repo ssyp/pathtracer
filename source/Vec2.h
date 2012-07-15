fa66575d27b6308e13652421047dc6021c709ade
Moress <lenta96@mail.ru>
2012-07-15 16:12:18 +0700

    Added more operators in Vek2 and Vek3 class. Fixed bugs in Ray.h

--- source/Vec2.h
+++ source/Vec2.h
@@ -32,12 +32,70 @@ class Vec2 {
 	}
 
 	void normalize() {
-		float l=1.0f / getLength();
+		float l = 1.0f / getLength();
 		x * = l; y * = l;
 	}
 
 	T dot(const Vec2<T> & vec2) const {
 		return (x * vec2.x + y * vec2.y);
 	}
+
+	bool operator < (const Vec2<T> & vec2) const
+	{
+		if (x + y + z < vec2.x + vec2.y) 
+		{
+			return 1;
+		}
+		return 0;
+	}
+
+	bool operator > (const Vec2<T> & vec2) const
+	{
+		if (x + y + z < vec2.x + vec2.y) 
+		{
+			return 0;
+		}
+		return 1;
+	}
+
+	Vec2<T> operator += (const Vec2<T> & vec2) const {
+		Vec2<T> result;
+		result.x = x + vec2.x;
+		result.y = y + vec2.y;
+		return result;
+	}
+
+	Vec2<T> operator -= (const Vec2<T> & vec2) const {
+		Vec2<T> result;
+		result.x = x - vec2.x;
+		result.y = y - vec2.y;
+		return result;
+	}
+
+	Vec2<T> operator *= (const T & k) const {
+		Vec2<T> result;
+		result.x = x * k;
+		result.y = y * k;
+		return result;
+	}
+
+	bool operator == (const Vec2<T> & vec2) const
+	{
+		if (x + y + z = vec2.x + vec2.y) 
+		{
+			return 0;
+		}
+		return 1;
+	}
+
+	bool operator != (const Vec2<T> & vec2) const
+	{
+		if (x + y + z != vec2.x + vec2.y) 
+		{
+			return 0;
+		}
+		return 1;
+	}
 };
 #endif
+

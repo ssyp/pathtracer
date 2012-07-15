048aeb6266e8ac8f7d2c615696a0510dd122d3da
Twice <nikestrike0@gmail.com>
2012-07-15 20:13:47 +0700

    added some refactoring with rendering

--- source/Camera.cpp
+++ source/Camera.cpp
@@ -1,29 +1,23 @@
 #include "Camera.h"
 
-Camera::Camera(Vec3<float>& pos, Vec3<float>& d, int x, int y)
-{
+Camera::Camera(Vec3<float>& pos, Vec3<float>& d, int x, int y) {
 	position = pos;
 	direct  = d;
 	dpiX = x;
 	dpiY = y;
 	screenSizeX = 1024 / dpiX;
 	screenSizeY = 600 / dpiY;
-	
 }
 
-Vec3<float> Camera::getPos() const
-{
+Vec3<float> Camera::getPos() const {
 	return position;
 }
-Vec3<float> Camera::getDir() const
-{
+Vec3<float> Camera::getDir() const {
 	return direct;
 }
-int Camera::getDpiX() const
-{
+int Camera::getDpiX() const {
 	return dpiX;
 }
-int Camera::getDpiY() const
-{
+int Camera::getDpiY() const {
 	return dpiY;
 }

f14fb8ce14a7aba9383d85f5fce9102d7b04ba3a
Moress <lenta96@mail.ru>
2012-07-16 13:30:30 +0700

    Added namespace to MathUtils.h, Ray.h, Ray.cpp, Vec2.h, Vec3.h

--- source/Vec2.h
+++ source/Vec2.h
@@ -1,131 +1,133 @@
 #ifndef __VEC2_H__ 
 #define __VEC2_H__
 
-template <class T>
-class Vec2 {
-public:
+namespace Math {
+	template <class T>
+	class Vec2 {
+	public:
 		T x;
 		T y;
 
-	Vec2() {
-		x = 0;
-		y = 0;
-	}
+		Vec2() {
+			x = 0;
+			y = 0;
+		}
 
-	Vec2(T newX, T newY, T newZ) {
-		x = newX;
-		y = newY;
-	}
+		Vec2(T newX, T newY, T newZ) {
+			x = newX;
+			y = newY;
+		}
 
-	Vec2<T> operator * (const Vec2<T> & vec) const {
-		Vec2<T> result;
-		result.x = x * vec.x;
-		result.y = y * vec.y;
-		return result;
-	}
+		Vec2<T> operator * (const Vec2<T> & vec) const {
+			Vec2<T> result;
+			result.x = x * vec.x;
+			result.y = y * vec.y;
+			return result;
+		}
 
-	Vec2(const Vec2<T> & vec) {
-		x = vec.x;
-		y = vec.y;
-	}
+		Vec2(const Vec2<T> & vec) {
+			x = vec.x;
+			y = vec.y;
+		}
 
-	Vec2<T> operator = () const {
-	Vec2<T> result;
-	result.x = vec.x;
-	result.y = vec.y;
-	return result;
-	}
+		Vec2<T> operator = () const {
+			Vec2<T> result;
+			result.x = vec.x;
+			result.y = vec.y;
+			return result;
+		}
 
-	Vec2<T> operator + (const Vec2<T> & vec2) const {
-		Vec2<T> result;
-		result.x = x + vec2.x;
-		result.y = y + vec2.y;
-		return result;
-	}
+		Vec2<T> operator + (const Vec2<T> & vec2) const {
+			Vec2<T> result;
+			result.x = x + vec2.x;
+			result.y = y + vec2.y;
+			return result;
+		}
 
-	Vec2<T> operator - (const Vec2<T> & vec2) const {
-		Vec2<T> result;
-		result.x = x - vec2.x;
-		result.y = y - vec2.y;
-		return result;
-	}
+		Vec2<T> operator - (const Vec2<T> & vec2) const {
+			Vec2<T> result;
+			result.x = x - vec2.x;
+			result.y = y - vec2.y;
+			return result;
+		}
 
-	Vec2<T> operator * (const T & k) const {
-		Vec2<T> result;
-		result.x = x * k;
-		result.y = y * k;
-		return result;
-	}
+		Vec2<T> operator * (const T & k) const {
+			Vec2<T> result;
+			result.x = x * k;
+			result.y = y * k;
+			return result;
+		}
 
-	float getLength() const {
-		return static_cast<float> (sqrt (x * x + y * y));
-	}
+		float getLength() const {
+			return static_cast<float> (sqrt (x * x + y * y));
+		}
 
-	void normalize() {
-		float l = 1.0f / getLength();
-		x * = l; y * = l;
-	}
+		void normalize() {
+			float l = 1.0f / getLength();
+			x * = l; y * = l;
+		}
 
-	T dot(const Vec2<T> & vec2) const {
-		return (x * vec2.x + y * vec2.y);
-	}
+		T dot(const Vec2<T> & vec2) const {
+			return (x * vec2.x + y * vec2.y);
+		}
 
-	bool operator < (const Vec2<T> & vec2) const
-	{
-		if (x + y + z < vec2.x + vec2.y) 
+		bool operator < (const Vec2<T> & vec2) const
 		{
+			if (x + y + z < vec2.x + vec2.y) 
+			{
+				return 1;
+			}
+			return 0;
+		}
+
+		bool operator > (const Vec2<T> & vec2) const
+		{
+			if (x + y + z < vec2.x + vec2.y) 
+			{
+				return 0;
+			}
 			return 1;
 		}
-		return 0;
-	}
 
-	bool operator > (const Vec2<T> & vec2) const
-	{
-		if (x + y + z < vec2.x + vec2.y) 
-		{
-			return 0;
+		Vec2<T> operator += (const Vec2<T> & vec2) const {
+			Vec2<T> result;
+			result.x = x + vec2.x;
+			result.y = y + vec2.y;
+			return result;
 		}
-		return 1;
-	}
 
-	Vec2<T> operator += (const Vec2<T> & vec2) const {
-		Vec2<T> result;
-		result.x = x + vec2.x;
-		result.y = y + vec2.y;
-		return result;
-	}
-
-	Vec2<T> operator -= (const Vec2<T> & vec2) const {
-		Vec2<T> result;
-		result.x = x - vec2.x;
-		result.y = y - vec2.y;
-		return result;
-	}
-
-	Vec2<T> operator *= (const T & k) const {
-		Vec2<T> result;
-		result.x = x * k;
-		result.y = y * k;
-		return result;
-	}
-
-	bool operator == (const Vec2<T> & vec2) const
-	{
-		if (x + y + z = vec2.x + vec2.y) 
-		{
-			return 0;
+		Vec2<T> operator -= (const Vec2<T> & vec2) const {
+			Vec2<T> result;
+			result.x = x - vec2.x;
+			result.y = y - vec2.y;
+			return result;
 		}
-		return 1;
-	}
 
-	bool operator != (const Vec2<T> & vec2) const
-	{
-		if (x + y + z != vec2.x + vec2.y) 
-		{
-			return 0;
+		Vec2<T> operator *= (const T & k) const {
+			Vec2<T> result;
+			result.x = x * k;
+			result.y = y * k;
+			return result;
 		}
-		return 1;
+
+		bool operator == (const Vec2<T> & vec2) const
+		{
+			if (x + y + z = vec2.x + vec2.y) 
+			{
+				return 0;
+			}
+			return 1;
+		}
+
+		bool operator != (const Vec2<T> & vec2) const
+		{
+			if (x + y + z != vec2.x + vec2.y) 
+			{
+				return 0;
+			}
+			return 1;
+		}
 	}
-};
+}
 #endif
 

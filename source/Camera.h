406c552da17c1eaeead510e57f7f9977656703d7
Twice <nikestrike0@gmail.com>
2012-07-16 14:36:07 +0700

    some fixing with rendering

--- source/Camera.h
+++ source/Camera.h
@@ -2,21 +2,35 @@
 #define _CAMERA_H_
 
 #include "vec3.h"
+#include "Ray.h"
+using namespace Math;
 
 class Camera {
 	public:
 		Camera(Vec3<float> & pos, Vec3<float> & d, float angel, int x, int y);
+
+		void setPos(Vec3<float> & vec);
 		Vec3<float> getPos() const;
+
+		void setDir(Vec3<float> & vec);
 		Vec3<float> getDir() const;
+
 		float getAngel() const;
+
 		int getDpiX() const;
 		int getDpiY() const;
+
+		Ray & genRay(int x, int y, int z);
+
 	private:
 		Vec3<float> position;
 		Vec3<float> direct;
+
 		int dpiX;
 		int dpiY;
+
 		int screenSizeX, screenSizeY;
+		
 		float angel;
 };
 
--- source/MonteCarloImage.h
+++ source/MonteCarloImage.h
@@ -1,13 +1,20 @@
 #ifndef _MONTE_CARLO_
 #define _MONTE_CARLO_
 #include "vec3.h"
+using namespace Math;
 
 class MonteCarloImage {
 	public:
 		MonteCarloImage(int x, int y);
+		~MonteCarloImage();
+
 		void add(int x, int y, Vec3<float> color);
 		Vec3<float> & get(int x, int y) const;
+		
+		void save() const;
+
 	private:
+		int x, y;
 		Vec3<float>** pixels;
 
 };

6b3de9f0c5acb86b1a703d7184698652d8fb7e33
Ilya <ilya@lyamkin.co.cc>
2012-07-16 15:00:16 +0700

    Add class IMaterial

---
+++ source/IMaterial.h
@@ -0,0 +1,13 @@
+#ifndef __IMATERIAL_H__
+#define __IMATERIAL_H__
+
+using namespace Math;
+
+class IMaterial {
+	public:
+		virtual float getBRDF(Vec3<float> in, Vec3<float> out, Vec3<float> n) = 0;
+		virtual Vec3<float> interact(Vec3<float> in, Vec3<float> ip, Vec3<float> n) = 0;
+};	
+
+
+#endif

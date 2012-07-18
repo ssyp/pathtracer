094c955022ff99bb78f161510773c24914bda227
Uchiro <uchiro.me@gmail.com>
2012-07-18 14:10:13 +0700

    Implemented interact ins simple material

--- source/SimpleMaterial.cpp
+++ source/SimpleMaterial.cpp
@@ -1,11 +1,10 @@
 #include "SimpleMaterial.h"
+#include "MathUtils.h"
 
 float SimpleMaterial::getBRDF(const Vec3<float> & in, const Vec3<float> & out, const Vec3<float> & n) const {
 	return 1.0f;
 }
 
 Vec3<float> SimpleMaterial::interact(const Vec3<float> & in, const Vec3<float> & ip, const Vec3<float> & n) const {
-	// interact == случайный луч по полусфере от нормали
-	Vec3<float> vec;
-	return vec;
+	return GetRandomPointOnHemisphere(n);
 }

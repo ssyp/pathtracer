ed4774ebefe12701120c91cee77c78870e59a3ee
Ilujik <ilya@lyamkin.co.cc>
2012-07-19 11:11:16 +0700

    Change SimpleMaterial

--- source/SimpleMaterial.h
+++ source/SimpleMaterial.h
@@ -2,11 +2,17 @@
 #define __SIMPLEMATERIAL_H__
 
 #include "IMaterial.h"
+#include "Block.h"
 
 class SimpleMaterial : public IMaterial {
+	private:
+		Vec3<float> reflectance;
+		Vec3<float> emittance;
 	public:
 		float getBRDF(const Vec3<float> & in, const Vec3<float> & out, const Vec3<float> & n) const;
 		Vec3<float> interact(const Vec3<float> & in, const Vec3<float> & ip, const Vec3<float> & n) const;
+		Vec3<float> getColor(const Vec3<float> & color);
+		void init(const Block & block);
 };
 
 #endif

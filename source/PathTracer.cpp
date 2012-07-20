62718c39ebe21dc6f38cba87620b23472338e3a9
Ilujik <ilya@lyamkin.co.cc>
2012-07-20 19:31:41 +0700

    Deleted warnings in PathTracer.cpp

--- source/PathTracer.cpp
+++ source/PathTracer.cpp
@@ -6,16 +6,20 @@ int main(int argc, char* argv[])
 {
 	std::string scene, output;
 	float iterations, samples;
+	char* sceneC = "--scene";
+	char* iterationsC = "--iterations";
+	char* samplesC = "--samples";
+	char* outputC = "--output";
 	for(int i = 0; i < argc; i++) {
-		if(argv[i] == "--scene") {
+		if(argv[i] == sceneC) {
 			scene = argv[i + 1];
 			scene = scene.substr(1, (scene.length() - 2));
 		}
-		if(argv[i] == "--iterations") 
+		else if(argv[i] == iterationsC) 
 			iterations = static_cast<float>(atof(argv[i + 1]));
-		if(argv[i] == "--samples") 
+		else if(argv[i] == samplesC) 
 			samples = static_cast<float>(atof(argv[i + 1]));
-		if(argv[i] == "--output") {
+		else if(argv[i] == outputC) {
 			output = argv[i + 1];
 			output = output.substr(1, (output.length() - 2));
 		}

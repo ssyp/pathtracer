b57ec3d8f0c525cebba7c8378841e1efa9e83ed3
Twice <nikestrike0@gmail.com>
2012-07-21 17:26:31 +0700

    changes

--- source/MirrorMaterial.cpp
+++ source/MirrorMaterial.cpp
@@ -11,12 +11,17 @@ Vec3<float> MirrorMaterial::getColor(const Vec3<float> & color) {
 
 Vec3<float> MirrorMaterial::interact(const Vec3<float> & in, const Vec3<float> & ip, const Vec3<float> & n) const {
 	Vec3<float> nin = in*(-1);
-	nin.normalize();
+	//nin.normalize();
 	float cos = dot(nin, n);
 	//float sin = sqrt(1-cos*cos);
 	Vec3<float> v1 = n*(2.0*cos);
 	Vec3<float> v =  v1+nin;
 	return v;
+	/**Vec3<float> nin = in;
+	nin.normalize();
+	float cos = dot(nin*(-1), n);
+	Vec3<float> v = (in * sqrt(1-cos*cos))*2.0;
+	return v - in;**/
 }
 
 void MirrorMaterial::init(const Block & block) {
